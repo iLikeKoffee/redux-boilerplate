e44106adf88909c390a44181aaf07f1dc42aa97c
alexeysidash <alexeysidash@gmail.com>
2016-02-01 13:49:26 +0300

    Student page added

--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react'
-import Header from './header'
-import User from './about-user'
+import Header from '../components/header'
 import Teacher from './teacher/teacher'
 import TeacherLecture from './techer-lecture/teacher-lecture'
-import Student from './student/student'
-import Footer from './footer'
+import Student from './student/index'
+import Footer from '../components/footer'
+import Logo from '../components/ksu-logo'
 
-const logoPath = require('../../img/logo.png')
 
 export default class Main extends Component {
 
@@ -15,17 +14,7 @@ export default class Main extends Component {
       <div>
         <div className='wrapper'>
           <Header/>
-          <div className='subhead shad'>
-            <div className='container'>
-              <div className='row'>
-                <User/>
-                <div className='col-md-4'>
-                  <a href='' className='logo'> <img src={logoPath}/> <span>Портал дистанционного образования КГУ</span>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Logo />
 
           <Student/>
 
--- src/js/pages/student/index.js
+++ src/js/pages/student/index.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
 import Task from './task-list'
 import CourseList from './course-list'
+import Header from '../../components/header'
+import Logo from '../../components/ksu-logo'
+import Footer from '../../components/footer'
 
 
 const courses = [
@@ -27,13 +30,20 @@ const articles = [
 export default class Student extends Component {
   render () {
     return (
-      <div className='pt40'>
-        <div className='container'>
-          <div className='row'>
-            <Task/>
-            <CourseList courses={courses} articles={articles}/>
+      <div>
+        <div className='wrapper'>
+          <Header/>
+          <Logo />
+          <div className='pt40'>
+            <div className='container'>
+              <div className='row'>
+                <Task/>
+                <CourseList courses={courses} articles={articles}/>
+              </div>
+            </div>
           </div>
         </div>
+        <Footer/>
       </div>
     )
   }
