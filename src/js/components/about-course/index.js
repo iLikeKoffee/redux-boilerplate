7b89fcc276ed3c3653a611c2647e79ec3db04198
alexeysidash <alexeysidash@gmail.com>
2016-03-10 14:36:00 +0300

    get labels upper.

--- src/js/components/about-course/index.js
+++ src/js/components/about-course/index.js
@@ -10,15 +10,15 @@ export default class AboutCourse extends Component {
     const items = newsList
       .map(c => (
         <div className='list-item number'>
-          <h1 className='bubble'>
-            <div>
+          <div className='bubble'>
+            <div className='clearfix'>
               <div className='cover' style={{backgroundImage: `url(${fond})`}}/>
               <div className="data-right">
-                <div className='title'> {c.title}</div>
-                <div className='text'> {c.text}</div>
+                <h1 className='title'>{c.title}</h1>
+                <p className='text'>{c.text}</p>
               </div>
             </div>
-          </h1>
+          </div>
           <a href="">
             <div className='teacher'>
               <div className='img-teacher'><img src={ImgPath} width='100'/></div>
