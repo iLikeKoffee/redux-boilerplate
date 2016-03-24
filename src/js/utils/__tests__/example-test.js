c683e5360156ffa48b829af78335a6d3fc15c9ad
alexeysidash <alexeysidash@gmail.com>
2016-03-24 14:13:46 +0300

    Added mocha tests instead of jest.

--- src/js/utils/__tests__/example-test.js
+++
@@ -1,9 +0,0 @@
-jest.dontMock('../example')
-const {increment, decrement} = require('../example')
-
-describe('example', () => {
-  it('Increments and decrements numbers', () => {
-    expect(increment(2)).toBe(3)
-    expect(decrement(2)).toBe(1)
-  })
-})
---
+++ src/js/utils/test/index.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+import 'should';// eslint-disable-line
+
+describe('Test', () => {
+  describe('Assertions', () => {
+    it('assertions should work', () => {
+      (1).should.be.exactly(1)
+    })
+  })
+})
